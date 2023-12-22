34734f83da41c5e78c6dd637bc986d7bad272695
sevonj <100710152+sevonj@users.noreply.github.com>
2023-12-22 12:36:04 +0200

    Move icons to assets/icons

--- src/addons/worldeditor/classes/we_consts.gd
+++ src/addons/worldeditor/classes/we_consts.gd
@@ -5,6 +5,6 @@ const GIZMO_ICON_SIZE: float = .024
 
 const PATHJUNC_GIZMO_SIZE: float = .2
 
-const ICON_PATH_CONNECT = preload("res://addons/worldeditor/icons/connect.png")
-const ICON_PATH_DISCONNECT = preload("res://addons/worldeditor/icons/disconnect.png")
-const ICON_PATH = preload("res://addons/worldeditor/icons/path.png")
+const ICON_PATH_CONNECT = preload("res://addons/worldeditor/assets/icons/connect.png")
+const ICON_PATH_DISCONNECT = preload("res://addons/worldeditor/assets/icons/disconnect.png")
+const ICON_PATH = preload("res://addons/worldeditor/assets/icons/path.png")
